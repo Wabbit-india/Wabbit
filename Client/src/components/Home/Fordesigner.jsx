259bff83432efee6546818ea1c56874a81544cf0
Mohammed Hussain <mohammedhussain7339@gmail.com>
2024-10-05 13:02:16 +0530

    push hussain

--- Client/src/components/Home/Fordesigner.jsx
+++ Client/src/components/Home/Fordesigner.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Fordesigner() {
   return (
-    <div>
+    <div className='bg-red-200 w-full h-[100vh]'>
       <h1>
-        hello from Fordesigner
+        hello from Fordesignersss
       </h1>
     </div>
   )
