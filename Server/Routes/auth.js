0bbaec26a92dce03fdad3761eef6f90f9d511ffa
amitsevda2303 <rohitkmt88@gmail.com>
2024-10-17 22:34:17 +0530

    create lofin route and refine register route in backend by amit

--- Server/Routes/auth.js
+++ Server/Routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser } from "../Controller/auth.js";
+import { createUser, loginUser } from "../Controller/auth.js";
 
 const router = express.Router();
 
 try {
-    router.post("/register", createUser)
+    router.post("/register", createUser);
+    router.post("/login", loginUser);
 } catch (error) {
     console.log("Intrenal server error at auth route 🔴 ", error);
 }
